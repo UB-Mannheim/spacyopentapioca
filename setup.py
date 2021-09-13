44b6d1014430b18049602e25bb2c728b6a15adfd
Renat Shigapov <57352291+shigapov@users.noreply.github.com>
2021-09-13 11:20:15 +0200

    set v0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spacyopentapioca",
-    version='0.1.1',
+    version='0.1.2',
     author="Renat Shigapov",
     license="MIT",
     description="A spaCy wrapper of OpenTapioca for named entity linking on Wikidata.",
